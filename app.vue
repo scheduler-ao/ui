b4f046cba85d3abd7eb94b6fbd7a6cd264177b67
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2024-03-15 23:15:55 +0300

    fix shadcn-vue issue

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <div class="h-screen bg-slate-900 p-10 text-center">
+      <Button>Click me</Button>
+    </div>
   </div>
 </template>
